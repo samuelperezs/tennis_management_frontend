7f0e8489bc6c98635da81da88f3126e81ff3dfe1
samuelperezs <infra@samuelperez.cl>
2025-04-25 17:42:06 -0400

    Add basic routes configurations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, effect, inject, OnInit } from '@angular/core';
+import {
+  NavigationEnd,
+  NavigationStart,
+  Router,
+  RouterOutlet,
+} from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { HttpClient } from '@angular/common/http';
+import { UserService } from './services/user.service';
+import { devEnvironment } from '../environments/environment.development';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'front';
+export class AppComponent implements OnInit {
+  enviroment = devEnvironment;
+  private _httpClient = inject(HttpClient);
+  userService = inject(UserService);
+  router = inject(Router);
+  previousUrl: string = '';
+
+  constructor() {
+    effect(() => this.userService.isuserLoggedIn() != null ? (this.userService.isuserLoggedIn() ? this.checkUser() : this.router.navigateByUrl('/login')) : '');
+  }
+
+  ngOnInit(): void {
+  }
+
+  checkUser(): void {
+    this._httpClient.get<any>(`${this.enviroment.url}/user`).subscribe({
+      next: (res) => {
+        console.log('response', res);
+        this.userService.currentUser.set(res.user);
+      },
+      error: () => {
+        this.userService.currentUser.set(null);
+        this.router.navigateByUrl('/login');
+      },
+    });
+  }
 }
